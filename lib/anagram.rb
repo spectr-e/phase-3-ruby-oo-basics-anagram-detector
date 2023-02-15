8979b221c00dbcf55554be487d13bc45bde7c16a
spectr-e <devs.josia@gmail.com>
2023-02-15 14:27:10 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagram_arr = []
+    word_sort = @word.chars.sort.join
+    anagrams.each do |anagram|
+      anag_sort = anagram.chars.sort.join
+      if word_sort == anag_sort
+        anagram_arr << anagram
+      end
+    end
+    anagram_arr
+  end
+end
